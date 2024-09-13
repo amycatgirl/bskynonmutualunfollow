3ee0e50b4d2d13bf1a825b5d55469c20b11d3455
Amy <amy@amogus.cloud>
2024-09-13 08:47:07 -0400

    chore: format file

--- index.js
+++ index.js
@@ -8,30 +8,37 @@ import { inspect } from "node:util";
 let agent;
 
 async function askUserPDS() {
-    /** @type {string} */
-    const output = await input("PDS URL: ") ?? "https://bsky.social" // Default to BSKY AppService;
+  /** @type {string} */
+  const output = (await input("PDS URL: ")) ?? "https://bsky.social"; // Default to BSKY AppService;
 
-    const credSession = new CredentialSession({
-        service: output
-    })
+  const credSession = new CredentialSession({
+    service: output,
+  });
 
-    agent = new Agent(credSession)
+  agent = new Agent(credSession);
 }
 
 async function promptSessionStart() {
-    const identifier = await input("Identifier: ")
-    const password = await input("Password: ")
-    if (!identifier || !password) throw new Error("Missing identifier and/or Password")
+  const identifier = await input("Identifier: ");
+  const password = await input("Password: ");
+  if (!identifier || !password)
+    throw new Error("Missing identifier and/or Password");
 
-    await agent.login({
-        identifier,
-        password
-    })
+  await agent.login({
+    identifier,
+    password,
+  });
 
-    const { data: profileData } = await agent.getProfile({ actor: agent.did })
+  const { data: profileData } = await agent.getProfile({ actor: agent.did });
 
-    console.info(`Logged in as ${profileData.displayName ?? profileData.handle} ${profileData.displayName && `(@${profileData.handle})`}`)
-    console.info(`Followers: ${profileData.followersCount} | Following: ${profileData.followsCount}`)
+  console.info(
+    `Logged in as ${profileData.displayName ?? profileData.handle} ${
+      profileData.displayName && `(@${profileData.handle})`
+    }`
+  );
+  console.info(
+    `Followers: ${profileData.followersCount} | Following: ${profileData.followsCount}`
+  );
 }
 
 /**
@@ -39,20 +46,27 @@ async function promptSessionStart() {
  * @returns {Promise<[import("@atproto/api").AppBskyActorDefs.ProfileView[], import("@atproto/api").AppBskyActorDefs.ProfileView[]]>
  */
 async function getFollowersAndFollowing() {
-    const { data: followersData } = await agent.getFollowers({ actor: agent.did })
-    const { data: followingData } = await agent.getFollows({ actor: agent.did })
+  const { data: followersData } = await agent.getFollowers({
+    actor: agent.did,
+  });
+  const { data: followingData } = await agent.getFollows({ actor: agent.did });
 
-    return [followersData.followers, followingData.follows]
+  return [followersData.followers, followingData.follows];
 }
 
-await askUserPDS()
-await promptSessionStart()
-const [followers, follows] = await getFollowersAndFollowing()
+await askUserPDS();
+await promptSessionStart();
+const [followers, follows] = await getFollowersAndFollowing();
 
 for (const follow of follows) {
-    const foundRelatedFollower = followers.find((follower) => follower.did === follow.did) // Find by DID
+  const foundRelatedFollower = followers.find(
+    (follower) => follower.did === follow.did
+  ); // Find by DID
 
-    if (foundRelatedFollower) continue; // skip for now
+  if (foundRelatedFollower) continue; // skip for now
 
-    console.log("Found NonMutual Follow:", follow.displayName ?? "@" + follow.handle)
+  console.log(
+    "Found NonMutual Follow:",
+    follow.displayName ?? "@" + follow.handle
+  );
 }
