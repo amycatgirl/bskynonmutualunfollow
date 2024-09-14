d745c672c866a2a7acacfdeb90e731a1a569007b
Amy <amy@amogus.cloud>
2024-09-14 13:20:56 -0400

    feat: finalize script functionality

---
+++ common/delay.js
@@ -0,0 +1,3 @@
+const delay = (amount) => new Promise((resolve) => setTimeout(resolve, amount * 1000))
+
+export { delay }
--- index.js
+++ index.js
@@ -1,38 +1,58 @@
-import { Agent, CredentialSession } from "@atproto/api";
+import { AtpAgent, CredentialSession } from "@atproto/api";
 import { input } from "./common/stdio.js";
 import { inspect } from "node:util";
+import { writeFile, open, stat } from "node:fs/promises";
+import { delay } from "./common/delay.js";
+
+import readlineSync from "readline-sync";
 
 /**
  * @type {import("@atproto/api").AtpAgent}
  */
 let agent;
-
-async function askUserPDS() {
-  /** @type {string} */
-  const output = (await input("PDS URL: ")) ?? "https://bsky.social"; // Default to BSKY AppService;
-
-  const credSession = new CredentialSession({
-    service: output,
-  });
-
-  agent = new Agent(credSession);
-}
-
 async function promptSessionStart() {
-  const identifier = await input("Identifier: ");
-  const password = await input("Password: ");
-  if (!identifier || !password)
-    throw new Error("Missing identifier and/or Password");
-
-  await agent.login({
-    identifier,
-    password,
+  const service = await input("Service (If unsure, use https://bsky.social): ");
+  agent = new AtpAgent({
+    service,
+    persistSession: async (ev, session) => {
+      console.log("Saving session to bskysession.json...");
+      await writeFile("bskysession.json", JSON.stringify(session)).then(() =>
+        console.log("Done!")
+      );
+    },
   });
+  const sessionExists = await stat("bskysession.json")
+    .then(() => true)
+    .catch(() => false);
+
+  if (sessionExists) {
+    console.log("Found session! Resuming...");
+    const sessionDataFile = await open("bskysession.json");
+    const data = JSON.parse(
+      await sessionDataFile.readFile({ encoding: "utf8" })
+    );
+
+    // Close the file as this information isn't needed anymore
+    await sessionDataFile.close();
+    await agent.resumeSession(data);
+  } else {
+    const identifier = await input("Identifier: ");
+    const password = readlineSync.question("Password: ", {
+      hideEchoBack: true,
+      mask: "*",
+    });
+    if (!identifier || !password)
+      throw new Error("Missing identifier and/or Password");
+    await agent.login({
+      identifier,
+      password,
+    });
+  }
 
   const { data: profileData } = await agent.getProfile({ actor: agent.did });
 
   console.info(
-    `Logged in as ${profileData.displayName ?? profileData.handle} ${
+    `Logged in as ${profileData.displayName || "@" + profileData.handle} ${
       profileData.displayName && `(@${profileData.handle})`
     }`
   );
@@ -54,19 +74,30 @@ async function getFollowersAndFollowing() {
   return [followersData.followers, followingData.follows];
 }
 
-await askUserPDS();
 await promptSessionStart();
 const [followers, follows] = await getFollowersAndFollowing();
 
-for (const follow of follows) {
+for await (const follow of follows) {
   const foundRelatedFollower = followers.find(
     (follower) => follower.did === follow.did
   ); // Find by DID
 
-  if (foundRelatedFollower) continue; // skip for now
+  if (foundRelatedFollower) continue;
 
-  console.log(
-    "Found NonMutual Follow:",
-    follow.displayName ?? "@" + follow.handle
-  );
+  console.log("Unfollowing", follow.displayName ?? "@" + follow.handle);
+  try {
+    await agent.deleteFollow(follow.viewer.following);
+  } catch (error) {
+    console.error("[ERROR] Could not delete follow", follow.handle + ".", "Stack trace included below:\n" + inspect(error, false, 1, true))
+  }
+
+  await delay(2); // Artificial delay, as I don't want to do ratelimiting :)
 }
+
+// Assume we are done, hopefully, no errors should be thrown
+
+console.log(
+  "Seems like everything is done! Check your follows on",
+  agent.serviceUrl.host,
+  "to make sure the users listed were unfollowed!"
+);
